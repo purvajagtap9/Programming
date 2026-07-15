d2497556480fb4853540154602ac1be75cfe4f81
purvajagtap9 <purvajagtap9@gmail.com>
2026-07-15 22:00:36 +0530

    Add files via upload

---
+++ C/Practice/program34.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void Display(int iNo)
+{  
+    int iCnt =0;
+    //Filter
+    if(iNo<0)
+    {
+        printf("Invalid Input\n");
+        return;
+    }
+    for(iCnt=iNo; iCnt>=0; iCnt--)  //decreasing number of iterations
+    {
+        printf("%d\n",iCnt);
+    }
+}  
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the Frequency : ");
+    scanf("%d",&iValue); 
+
+    Display(iValue);
+    
+    return 0;
+
+}
