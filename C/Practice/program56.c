ba55c48d8f6bcbcf93983d9ada264ce93163774e
purvajagtap9 <purvajagtap9@gmail.com>
2026-07-17 13:54:44 +0530

    Add files via upload

---
+++ C/Practice/program56.c
@@ -0,0 +1,29 @@
+//Input :  5
+//Output : 1 2 3 4  
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iNo; iCnt += 1)  //0+1 = 1 , 1+1 = 2 , 2+1 = 3 , 3+1 = 4
+    {
+        printf("%d\t",iCnt);
+
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d",&iValue);    
+
+    Display(iValue);
+    return 0;
+
+}
